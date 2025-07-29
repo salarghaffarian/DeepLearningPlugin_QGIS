cc9772d83d0403dff8fa17e8beef30c6f3b8a330
Salar Ghaffarian <salarghaffarian1363@gmail.com>
2025-07-29 02:01:37 -0600

    Update the main_ui.py module with its initial structure for creating the dockwidget

---
+++ ui/main_ui.py
@@ -0,0 +1,21 @@
+from qgis.PyQt.QtWidgets import QDockWidget, QTabWidget, QVBoxLayout, QWidget
+from .tab1 import Tab1Widget
+from .tab2 import Tab2Widget
+from .tab3 import Tab3Widget
+
+class DeepLearningDockWidget(QDockWidget):
+    def __init__(self, iface, parent=None):
+        super().__init__("Deep Learning Plugin", parent)
+        self.iface = iface
+        self.setObjectName("DeepLearningDockWidget")
+
+        main_widget = QWidget()
+        layout = QVBoxLayout(main_widget)
+
+        self.tabs = QTabWidget()
+        self.tabs.addTab(Tab1Widget(), "Model")
+        self.tabs.addTab(Tab2Widget(), "Inference")
+        self.tabs.addTab(Tab3Widget(), "Results")
+
+        layout.addWidget(self.tabs)
+        self.setWidget(main_widget)
